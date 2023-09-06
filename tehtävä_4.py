1086c3640a745aa02b5bacee17944888f79663a3
Miko Savolainen <70377423+mikosavolainen@users.noreply.github.com>
2023-09-06 14:19:01 +0300

    Update tehtävä_4.py

--- tehtävä_4.py
+++ tehtävä_4.py
@@ -1,6 +1,15 @@
 alkuluku = int(input("Anna luku:"))
 
-if alkuluku > 1 and alkuluku % i == 0:
+if alkuluku <= 1:
     print("Numero ei ole alkuluku")
 else:
-    print("Numero on alkuluku")
+    alkuluku_on_alkuluku = True
+    for i in range(2, alkuluku):
+        if alkuluku % i == 0:
+            alkuluku_on_alkuluku = False
+            break
+    
+    if alkuluku_on_alkuluku:
+        print("Numero on alkuluku")
+    else:
+        print("Numero ei ole alkuluku")
